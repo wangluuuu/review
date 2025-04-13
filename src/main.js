5f2d27e9417725b75f7b392a643150af25e96393
wanglu <578754295@qq.com>
2025-04-13 22:45:56 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
+//定义一个应用级的错误处理器，用来捕获所有子组件上的错误
+app.config.errorHandler = (err) => {
+    console.log(err)
+}
+
+//注册应用范围内可用的资源
+app.component()
