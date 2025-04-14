dbdbdeed98ad946309d3bcc2b8f65224b5e8f27f
wanglu <578754295@qq.com>
2025-04-15 00:47:13 +0800

    一个页面下挂载多个实例

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div>123456</div>
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import {createApp} from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+import TestComponent from "@/components/TestComponent.vue";
 
-const app = createApp(App)
-app.mount('#app')
+const app1 = createApp(App)
+app1.mount('#app')
+
+const app2 = createApp(TestComponent)
+app2.mount('#test')
 
 //定义一个应用级的错误处理器，用来捕获所有子组件上的错误
-app.config.errorHandler = (err) => {
+app1.config.errorHandler = (err) => {
     console.log(err)
 }
 
 //注册应用范围内可用的资源
-app.component()
+app1.component()
